eb523eb954631f0d70bc59a53f231c2fc272ef28
Joanna Buchała <joanna.b2@op.pl>
2018-04-12 22:20:48 +0200

    arrows

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 	var carouselList = $('#carousel ul');
 
+	function changeSlide() {
+		carouselList.animate({marginLeft: -600}, 900, moveFirstSlide);
+	}
+
+	function moveFirstSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: 0});
+	}
+
+	//setInterval(changeSlide, 3000);
+
 });
