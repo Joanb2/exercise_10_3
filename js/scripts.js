8667ac88c35e844a1cf81f81e493eeaee01a2f9e
Joanna Buchała <joanna.b2@op.pl>
2018-04-12 19:28:34 +0200

    add content

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-
-	setInterval(function(){
-
-		$('carousel ul').animate({marginLeft:'-600px'},2000,function(){
-			$(this).find('li:last').after($(this).find('li:first'));
-			$(this).css({marginLeft:0});
-		});
-	},4000);
+	var carouselList = $('#carousel ul');
 
 });
